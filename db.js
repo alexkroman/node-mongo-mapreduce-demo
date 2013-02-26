c90ed07441abcbd6d7048c7edc05c11ee7688346
Alex Kroman <alex@newrelic.com>
2013-02-26 14:42:57 -0800

    JSLint updates

--- db.js
+++ db.js
@@ -1,55 +1,47 @@
-var mongoose = require('mongoose')
-  , Schema = mongoose.Schema
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
-  body: {type : String, default : '', trim : true, set: toTags}
-  , user: {type : Schema.ObjectId, ref : 'User'}
-  , createdAt  : {type : Date, default : Date.now}
-  , tags : {type: [String]}
-})
+  body: {type : String},
+  tags : {type: [String]}
+});
 
-PostSchema.index({user:1, endedAt:1})
-
-function toTags(val) {
-  this.tags = val.match(/(#[A-Za-z0-9-_]+)/g)
-  return val; 
-}
+PostSchema.pre('save', function (next) {
+  this.tags = this.body.match(/(#[A-Za-z0-9-_]+)/g);
+  next();
+});
 
 PostSchema.statics.tags = function(callback) {
-
   var o = {};
 
   o.map = function() {
     if (!this.tags) {
       return;
     }
-     
     for (index in this.tags) {
       emit(this.tags[index], 1);
     }
-  }
+  };
 
   o.reduce = function(previous, current) {
 
-    var count = 0
+    var count = 0;
 
     for (index in current) {
-        count += current[index];
+      count += current[index];
     }
 
     return count;
-  }
+  };
 
-  o.out = 'tagResults'
+  o.out = 'tagResults';
 
   this.mapReduce(o, function (err, model, stats) {
     model.find().sort({'value.count': -1}).exec(callback);
   });
 
-}
+};
 
-mongoose.model('Post', PostSchema)
-
-mongoose.connect('mongodb://localhost/twitter_123')
-
-module.exports = { mongoose: mongoose }
+mongoose.model('Post', PostSchema);
+mongoose.connect('mongodb://localhost/twitter_1');
+module.exports = { mongoose: mongoose };
